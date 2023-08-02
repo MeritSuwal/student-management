399bcf69465799acd34ad5b7f6b778e47fbc2658
Aabid <71463876+aabiddd@users.noreply.github.com>
2023-08-02 21:55:35 +0545

    added the feature of command line argument

--- app.cpp
+++ app.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <cctype>
  
 using namespace std;
 
@@ -17,11 +18,31 @@ public:
     }
 
     void display() {
-        
+
     }
 };
  
 int main(int argc, char* argv[])
 {
+    if (argc != 2) {
+        cout << "Command Usage: ./app.exe <subject-name>" << endl;
+        return 1; //invalid number of arguments!
+    }
 
+    //checking whether the input subject is valid or not
+    char subjects[7][6] = {"OOP", "DL", "TOC", "ECT", "MATHS", "EDC", "EM"};
+    bool found = false;
+    for (int i = 0; i < 7; i++) {
+        if (strcmp(argv[1], subjects[i]) == 0) {
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "Enter a valid subject name!" << endl;
+        cout << "OOP, DL, TOC, ECT, MATHS, EDC, EM are the valid subjects!" << endl;
+        return 2; //invalid subject argument
+    }
+    
 }
