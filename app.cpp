b390387949e1fc4ee58d29d849afd713d345bf84
Merit Suwal <meritsuwal@gmail.com>
2023-08-02 22:43:41 +0545

    ..

--- app.cpp
+++ app.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
-#include <iomanip>    
+#include <cctype>
+#include <iomanip>
  
 using namespace std;
- 
+
 class Student {
     char name[40];
     char roll_no[13];
@@ -33,10 +34,105 @@ public:
         cout << setw(22) << setiosflags(ios::left) << name << setw(12) << setiosflags(ios::left)<< roll_no <<setiosflags(ios::left)<< setw(8) << faculty <<setiosflags(ios::left) << setw(5) <<setiosflags(ios::left)<< grade << setw(15) <<phone_no;
     }
 };
+
+void addUser(char *file_name);
+void modifyUser(char *file_name);
+void deleteUser(char *file_name);
+void displayID(char *file_name);
+void displayAll(char *file_name);
  
 int main(int argc, char* argv[])
 {
-Student s1;
-s1.getData();
-s1.display();
+    if (argc != 2) {
+        cout << "Command Usage: ./app.exe <subject-name>" << endl;
+        return 1; //invalid number of arguments!
+    }
+
+    //checking whether the input subject is valid or not
+    char subjects[7][6] = {"OOP", "DL", "TOC", "ECT", "MATHS", "EDC", "EM"};
+    bool found = false;
+    for (int i = 0; i < 7; i++) {
+        if (strcmp(argv[1], subjects[i]) == 0) {
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "Enter a valid subject name!" << endl;
+        cout << "OOP, DL, TOC, ECT, MATHS, EDC, EM are the valid subjects!" << endl;
+        return 2; //invalid subject argument
+    }
+
+    //file name:
+    char file_name[10];
+    sprintf(file_name, "%s.txt", argv[1]);
+    
+    //what do?
+    int n;
+
+    while(1) {
+        cout << "\nUser Modes: " << endl;
+        cout << "1. Add a Student" << endl;
+        cout << "2. Modify a Student's data" << endl;
+        cout << "3. Delete a Student's data" << endl;
+        cout << "4. Search a Student's data" << endl;
+        cout << "5. Display every student's data" << endl;
+        cout << "6. Exit" << endl;
+
+        cout << "Enter choice: ";
+        cin >> n;
+
+        //operate on these given mode numbers
+        switch(n) {
+            case 1:
+                addUser(file_name);
+                break;
+            case 2:
+                modifyUser(file_name);
+                break;
+            case 3:
+                deleteUser(file_name);
+                break;
+            case 4:
+                displayID(file_name);
+                break;
+            case 5:
+                displayAll(file_name);
+                break;
+            case 6:
+                return 0;
+            default:
+                cout << "Enter a valid choice!!" << endl;
+        }
+    }
+}
+
+void addUser(char *file_name) {
+    //TODO: add a single students data to the file
+    Student s;
+    
+    cout << "\nEnter the Student's info: " << endl;
+    s.getData();
+
+    ofstream file;
+    file.open(file_name);
+}
+
+void modifyUser(char *file_name) {
+    //TODO: modify student's complete data
+    //checkign chai roll no. le garne
+}
+
+void deleteUser(char *file_name) {
+    //TODO: delete a studnet's data, given: students' roll no
+}
+
+void displayID(char *file_name) {
+    //TODO: show the data of only one student, his/her roll no. should be mentioned by the user
+    // if not in the database, display appropriate message
+}
+
+void displayAll(char *file_name) {
+    //TODO: display each and every entry for that subject, in proper formatted manner
 }
