02c964de1e07180aa096701f50a2c46695c18165
Merit Suwal <meritsuwal@gmail.com>
2023-08-02 22:07:11 +0545

    added getData

--- app.cpp
+++ app.cpp
@@ -1,57 +1,39 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
 #include <string.h>
+#include <iomanip>    
  
 using namespace std;
 
 class Student {
-    string name;
-    string roll_no;
-    float gpa;
-    long int phone_no;
-    int* course_id;
-    int size;
+    char name[40];
+    char roll_no[13];
+    char faculty[15];
+    char grade;
+    char phone_no[11];
 
 public:
-    Student(string name=" ", string roll_no = 0, float gpa = 0.0 , long int phone_no = 0 , int array[] = 0, int n =0){
-
-        this-> name = name;
-        this-> roll_no = roll_no;
-        this-> gpa = gpa;
-        this-> phone_no;
-        size = n;
-        course_id = new int[size];
-        for (int i=0; i<size; i++){
-            course_id[i] = array[i];
-        }
-    }
-
-    void disp(){
-        cout << name << roll_no;
-        for (int i = 0; i<size; i++){
-        cout << course_id[i];
-    }
+    void getData() {
+        cout << "Enter your full name: ";
+        cin.getline (name, 40);
+        cin.ignore();
+        cout << "\nEnter your roll number: ";
+        cin >> roll_no ;
+        cout << "\nEnter your Faculty: ";
+        cin >> faculty;
+        cout << "\n Enter your Grade: ";
+        cin >> grade;
+        cout << "\Enter Phone number:";
+        cin >> phone_no;
 
     }
 
+    void display() {
+        cout << 
+    }
 };
  
-int main()
+int main(int argc, char* argv[])
 {
-    int course_num=2;
-    int *arr = new int[course_num];
-    string name;
-    cout << "Enter:";
-    cin >>name;
-
-    for (int i = 0; i<course_num; i++){
-        cin >> arr[i];
-    }
-
-    Student s1 (name, "67", 99, 90990, arr, course_num);
-    s1.disp();
-
-    delete[] arr;
-
 
 }
